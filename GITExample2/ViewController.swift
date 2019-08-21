e9ae5666f20c132cf29cd684d7a818b07ecc6de0
Mac <mac@Macs-Mac-mini.local>
2019-08-21 14:06:35 +0530

    Initial Commit

---
+++ GITExample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITExample2
+//
+//  Created by Girish Purandare on 21/08/19.
+//  Copyright © 2019 Girish Purandare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
